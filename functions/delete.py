2175fa62dde22d57f2a2deb7984f2c62f39d7ca4
pillowlibrary <thenamesethan.yt@gmail.com>
2024-10-19 18:15:06 -0700

    Update delete.py

--- functions/delete.py
+++ functions/delete.py
@@ -1,9 +1,8 @@
 def delete_file(file_path):
-    #hellonigga
     import os
     if os.path.isfile(file_path):
         try:
-            os.remove(file_path)  # Delete the file
+            os.remove(file_path)
             return f"**File deleted:** `{file_path}`"
         except Exception as e:
             return f"**Error:** Failed to delete file `{file_path}`: `{e}`"
