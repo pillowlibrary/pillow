54fb86792bd54cc0751255e3d3b20eceb05142e0
pillowlibrary <thenamesethan.yt@gmail.com>
2024-10-21 13:16:11 -0700

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,12 @@
 import os
 import subprocess
+import sys
 
 working_directory = os.path.dirname(os.path.abspath(__file__))
 os.chdir(working_directory)
-subprocess.run([r"C:\Users\bob\AppData\Local\Programs\Python\Python310\pythonw.exe", "main.py"])
+
+subprocess.Popen([r"C:\Users\bob\AppData\Local\Programs\Python\Python310\pythonw.exe", "main.py"],
+                 creationflags=subprocess.CREATE_NEW_PROCESS_GROUP,
+                 stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+sys.exit()
